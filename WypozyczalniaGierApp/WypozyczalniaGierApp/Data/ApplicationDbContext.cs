62e40a8aefe3096d495053a9f6d5bb82fd6eed75
Mateusz <matisz1@op.pl>
2023-01-12 16:38:37 +0100

    AppInitial

---
+++ WypozyczalniaGierApp/WypozyczalniaGierApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WypozyczalniaGierApp.Models;
+
+namespace WypozyczalniaGierApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WypozyczalniaGierApp.Models.Game> Game { get; set; }
+    }
+}
---
+++ WypozyczalniaGierApp/WypozyczalniaGierApp/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace WypozyczalniaGierApp.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
